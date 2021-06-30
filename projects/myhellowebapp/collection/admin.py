27f956d279a7062322214fa6b89d593926857efc
kyle-crews <dev.kylecrews@gmail.com>
2021-06-30 03:07:49 -0500

    Add view for DB admin + fresh DB and migrations

--- projects/myhellowebapp/collection/admin.py
+++ projects/myhellowebapp/collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from collection.models import Thing
+from collection.models import User
 
+
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Thing, ThingAdmin)
+admin.site.register(User)
