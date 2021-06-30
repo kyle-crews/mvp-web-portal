89f3b4016e3f034af3466996e74f11c3b3f8e1f2
kyle-crews <dev.kylecrews@gmail.com>
2021-06-30 02:16:05 -0500

    added Thing model and ran migration

--- projects/myhellowebapp/collection/models.py
+++ projects/myhellowebapp/collection/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
