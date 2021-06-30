36e45cad3bd576fb2b352d474396f73c1873f728
kyle-crews <dev.kylecrews@gmail.com>
2021-06-30 01:31:48 -0500

    added name view to home page

--- projects/myhellowebapp/collection/views.py
+++ projects/myhellowebapp/collection/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 def index(request):
     # define the variable
     number = 6
-    # pass the variable to the view
+    thing = "Thing name"
+    
     return render(request, 'index.html', {
         'number': number,
+        'thing': thing,
     })
